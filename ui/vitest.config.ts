e3920640772b2aec94e3060034b4a2b969600f78
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-26 17:10:16 +0100

    change sonar properties

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -17,15 +17,22 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      all: true,
+      include: ['**/*.{ts,tsx}'],
       exclude: [
         'node_modules/',
         'test/',
         '**/*.d.ts',
         '**/*.test.ts',
+        '**/*.test.tsx',
+        '**/*.spec.ts',
+        '**/*.spec.tsx',
         '**/*.config.ts',
         '**/types.ts',
-      ],
-      reportsDirectory: './coverage'
+        '.next/**',
+        'coverage/**'
+      ]
     },
   },
   esbuild: {
